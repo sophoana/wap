b5a6f9049c71c587eeff3e229748e8a9f39db523
Sophoana <span@orangetheoryfitness.com>
2018-07-01 23:14:45 -0400

    lab 1 completed

---
+++ aboutme.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>WAP, 986075!</title>
+  </head>
+  <body>
+  <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
+      <h5 class="my-0 mr-md-auto font-weight-normal">MUM: WAP Course</h5>
+      <nav class="my-2 my-md-0 mr-md-3">
+        <a class="p-2 text-dark" href="index.php">Home</a>
+      </nav>
+      <!-- <a class="btn btn-outline-primary" href="#">Sign up</a> -->
+    </div>
+
+    <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+      <h1 class="display-4">About me</h1>
+      <p class="lead">
+      My name is <b>Sophoana Pan </b>, a sport loving person from <b>Cambodia</b> who likes reinforcement challenge. 
+      <br /> That means challenge with yourself to create a better version of you. One of my favorite quotes is "Good stuffs come to those who sweat"
+      </p>
+    </div>
+
+    <div class="container">
+
+        <div>
+            <h5>Courses took in MUM</h5>
+            <ul>
+                <li>MPP</li>
+                <li>Software Engineering</li>
+                <li>Machine Learning</li>
+                <li>Algorithm</li>
+                <li>Big Data</li>
+            </ul>
+        </div>  
+
+        <div>
+            <h5>Favorite Movies, Books and TV Shows</h5>
+            <ul>
+                <li>
+                    <span class="badge badge-primary">Movie</span>
+                    The Shawshank Redemption - <a href="https://en.wikipedia.org/wiki/The_Shawshank_Redemption">Wikipedia Link</a>
+                </li>
+
+                <li>
+                    <span class="badge badge-secondary">Book</span>
+                    War and Peace by Leo Tolstoy
+                </li>
+
+                 <li>
+                    <span class="badge badge-info">TV Show</span>
+                    Prison Break
+                </li>
+            </ul>
+        </div>
+
+        <!-- Mood -->
+        
+        <div>
+            <h5>My mood</h5>
+            <div class="card-deck mb-3 text-center">
+                <div class="card mb-4 box-shadow">
+                    <div class="card-header">
+                        <h4 class="my-0 font-weight-normal">Happy</h4>
+                    </div>
+                    <div class="card-body">
+                        <img src="images/happy.jpg" alt="happy face">
+                    </div>
+                </div>
+
+                <div class="card mb-4 box-shadow">
+                    <div class="card-header">
+                        <h4 class="my-0 font-weight-normal">Sad</h4>
+                    </div>
+                    <div class="card-body">
+                        <img src="images/sad.jpg" alt="sad face">
+                    </div>
+                </div>
+            </div>
+        </div> 
+        
+        <!-- Neighbors -->
+        <div>
+            <h5>Fun fact about Neighbor</h5>
+            <ul>
+                <li>
+                    <span>Lu likes fishing</span>
+                </li>
+            </ul>
+        </div>
+      
+      <footer class="pt-4 my-md-5 pt-md-5 border-top">
+        <div class="row">
+          <div class="col-12 col-md">
+            <img class="mb-2" src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" alt="" width="24" height="24">
+            <small class="d-block mb-3 text-muted">&copy; 2017-2018</small>
+          </div>
+          <div class="col-6 col-md">
+            <h5>Resources</h5>
+            <ul class="list-unstyled text-small">
+              <!-- <li><a class="text-muted" href="#">Cool stuff</a></li>
+              <li><a class="text-muted" href="#">Random feature</a></li>
+              <li><a class="text-muted" href="#">Team feature</a></li>
+              <li><a class="text-muted" href="#">Stuff for developers</a></li>
+              <li><a class="text-muted" href="#">Another one</a></li>
+              <li><a class="text-muted" href="#">Last time</a></li> -->
+            </ul>
+          </div>
+
+          <div class="col-6 col-md">
+            <h5>Homework</h5>
+            <ul class="list-unstyled text-small">
+              <!-- <li><a class="text-muted" href="#">Resource</a></li>
+              <li><a class="text-muted" href="#">Resource name</a></li>
+              <li><a class="text-muted" href="#">Another resource</a></li>
+              <li><a class="text-muted" href="#">Final resource</a></li> -->
+            </ul>
+          </div>
+
+          <div class="col-6 col-md">
+            <h5>Lab</h5>
+            <ul class="list-unstyled text-small">
+              <li><a class="text-muted" href="aboutme.php">About Me</a></li>
+            </ul>
+          </div>
+        </div>
+      </footer>
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>WAP, 986075!</title>
+  </head>
+  <body>
+  <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
+      <h5 class="my-0 mr-md-auto font-weight-normal">MUM: WAP Course</h5>
+      <nav class="my-2 my-md-0 mr-md-3">
+        <a class="p-2 text-dark" href="aboutme.php">About me</a>
+      </nav>
+      <!-- <a class="btn btn-outline-primary" href="#">Sign up</a> -->
+    </div>
+
+    <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+      <h1 class="display-4">Welcome</h1>
+      <p class="lead">
+        Welcome to the Web Applications Programming DE course,  which will begin June 25th, 2018.
+      </p>
+    </div>
+
+    <div class="container">
+      
+      <footer class="pt-4 my-md-5 pt-md-5 border-top">
+        <div class="row">
+          <div class="col-12 col-md">
+            <img class="mb-2" src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" alt="" width="24" height="24">
+            <small class="d-block mb-3 text-muted">&copy; 2017-2018</small>
+          </div>
+          <div class="col-6 col-md">
+            <h5>Resources</h5>
+            <ul class="list-unstyled text-small">
+              <li> 
+                <p>
+                  <a href="http://validator.w3.org/check?uri=referer">
+                    <img src="http://www.w3.org/Icons/valid-xhtml11" alt="Valid XHTML 1.1" height="31" width="88" />
+                  </a>
+                </p>
+              </li>
+              <!-- <li><a class="text-muted" href="#">Cool stuff</a></li>
+              <li><a class="text-muted" href="#">Random feature</a></li>
+              <li><a class="text-muted" href="#">Team feature</a></li>
+              <li><a class="text-muted" href="#">Stuff for developers</a></li>
+              <li><a class="text-muted" href="#">Another one</a></li>
+              <li><a class="text-muted" href="#">Last time</a></li> -->
+            </ul>
+          </div>
+
+          <div class="col-6 col-md">
+            <h5>Homework</h5>
+            <ul class="list-unstyled text-small">
+              <!-- <li><a class="text-muted" href="#">Resource</a></li>
+              <li><a class="text-muted" href="#">Resource name</a></li>
+              <li><a class="text-muted" href="#">Another resource</a></li>
+              <li><a class="text-muted" href="#">Final resource</a></li> -->
+            </ul>
+          </div>
+
+          <div class="col-6 col-md">
+            <h5>Lab</h5>
+            <ul class="list-unstyled text-small">
+              <li><a class="text-muted" href="aboutme.php">About Me</a></li>
+            </ul>
+          </div>
+        </div>
+      </footer>
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
